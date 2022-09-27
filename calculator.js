1b2c3153e9542a4936976fa112b786a0992722ab
prasanthKminuscule <prasanth@minusculeTechnologies.com>
2022-09-27 16:17:01 +0530

    ADD function added

--- calculator.js
+++ calculator.js
@@ -5,8 +5,16 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class Calculator extends LightningElement {
  
 
- 
+ @track result=0;
 
+
+ handleAdd()
+ {
+     var num1=parseInt(this.template.querySelector('[data-id="num1"]').value);
+     var num2=parseInt(this.template.querySelector('[data-id="num2"]').value);
+     this.result=num1+num2;
+
+ }
 
 
 }
